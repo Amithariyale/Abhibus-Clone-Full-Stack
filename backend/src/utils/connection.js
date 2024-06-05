933b44d071b94fcdc83f5b1e808a86054f49c8bb
Amit Hariyale <amithariyale125@gmail.com>
2024-06-05 19:01:37 +0530

    Updating backend

--- backend/src/utils/connection.js
+++ backend/src/utils/connection.js
@@ -1,12 +1,9 @@
 const mongoose = require("mongoose");
-const { buses } = require("../../dummyData");
-const Bus = require("../models/busSchema");
 
 const connectDataBaseServer = async () => {
   try {
     await mongoose.connect(process.env.DB_HOST);
     console.log("connected to mongoDB", process.env.DB_HOST);
-    // Bus.insertMany(buses);
   } catch (error) {
     console.error("Connection to mongoDB failed", error.message);
   }
